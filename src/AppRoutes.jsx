b4669b86a092043eb1139d38771991b597eb2be0
Dayana Mariano <dayanaraquelle@gmail.com>
2024-06-11 23:57:47 -0300

    feat: autentificação com Navigate

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,15 +1,28 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { useContext } from "react";
+
 import HomePage from './Pages/HomePage'
 import LoginPage from "./Pages/LoginPage";
-import { AuthProvider } from "./contexts/auth";
+
+import { AuthProvider, AuthContext } from "./contexts/auth";
 
 const AppRoutes = () => {
+  const Private = ({children}) => {
+    const {authenticated} = useContext(AuthContext)
+
+    if(!authenticated) {
+      return <Navigate to="/login"/>
+    }
+
+    return children
+  }
+
   return (
     <Router>
       <AuthProvider >
         <Routes>
           <Route exact path="/login" element={<LoginPage/>} />
-          <Route exact path="/" element={<HomePage/>} />
+          <Route exact path="/" element={<Private><HomePage/></Private>}/>
         </Routes>
       </AuthProvider>
     </Router>
--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,17 +1,24 @@
 import { createContext, useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext()
 
 export const AuthProvider = ({children}) => {
+  const navigate = useNavigate()
   const [user, setUser] = useState(null)
 
   const login = (email, password) => {
     console.log('login auth', email, password)
-    setUser({id: '123', email})
+    if(password === "secret") {
+      setUser({id: '123', email})
+      navigate("/")
+    }
   }
 
   const logout = () => {
     console.log('logout')
+    setUser(null)
+    navigate("/login")
   }
 
   return (
